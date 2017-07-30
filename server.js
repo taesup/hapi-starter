959cfa7d68ee147dbc3e905acb3344eccfc41a0c
Edward Kim <ed@slickage.com>
2017-07-29 20:23:54 -1000

    Adding logging to console

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const Hapi = require('hapi');
-const inert = require('inert');
+const Inert = require('inert');
+const Good = require('good');
 const server = new Hapi.Server();
 
 const PORT = process.env.PORT || 9000;
@@ -10,9 +11,26 @@ server.connection({ port: PORT, host: 'localhost' });
 const routes = require('./routes');
 
 // static file serving
-server.register(inert)
+server.register(Inert)
+// logging
+.then(() => {
+  return server.register({
+    register: Good,
+    options: {
+      reporters: {
+        console: [{
+          module: 'good-squeeze',
+          name: 'Squeeze',
+          args: [{ response: '*', log: '*' }]
+        },
+        { module: 'good-console' },
+        'stdout']
+      }
+    }
+  });
+})
 // routes (must come after inert)
-.then(() => { server.route(routes); })
+.then(() => { return server.route(routes); })
 // start server
 .then(() => {
   server.start((err) => {
